dd32344657d2af02b4e05ee2dba656ed4fed56cf
aasthamiittal <aasthamittal2001@gmail.com>
2026-02-24 13:10:52 +0530

    changes in UI

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 
