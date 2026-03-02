823b83eb15b5511bc0bdfb1c00122d6b13e8fdaa
aasthamiittal <aasthamittal2001@gmail.com>
2026-03-02 23:04:41 +0530

    ;;

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_ORIGIN,
+    origin: process.env.FRONTEND_ORIGIN ,
     credentials: true,
   })
 );
